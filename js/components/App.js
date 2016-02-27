5b31020c23d29c2f604480df1ea861177dae2179
ербол <er361@mail.ru>
2016-02-27 18:06:09 +0600

    mebel list

--- js/components/App.js
+++ js/components/App.js
@@ -2,32 +2,39 @@ import React from 'react';
 import Relay from 'react-relay';
 
 class App extends React.Component {
+  showData(obj){
+    console.log(obj);
+  }
   render() {
+
     return (
       <div>
-        <h1>Widget list</h1>
-        <ul>
-          {this.props.viewer.widgets.edges.map(edge =>
-            <li key={edge.node.id}>{edge.node.name} (ID: {edge.node.id})</li>
-          )}
-        </ul>
+        <h1>Mebel list</h1>
+        {this.props.viewer.mebels.edges.map(edge =>
+          <li key={edge.node.id}>{edge.node.name}</li>
+        )}
       </div>
     );
   }
 }
 
 export default Relay.createContainer(App, {
+  prepareVariables(){
+    return {
+      limit: 100
+    }
+  },
   fragments: {
     viewer: () => Relay.QL`
-      fragment on User {
-        widgets(first: 10) {
-          edges {
-            node {
-              id,
-              name,
-            },
-          },
-        },
+      fragment on Viewer {
+        mebels(first: $limit){
+          edges{
+            node{
+              id
+              name
+            }
+          }
+        }
       }
     `,
   },
---
+++ relayschemaex.js
@@ -0,0 +1,50 @@
+import {
+  GraphQLInt,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql';
+
+const STORE = {
+  teas: [
+    {name: 'Earl Grey Blue Star', steepingTime: 5},
+    {name: 'Milk Oolong', steepingTime: 3},
+    {name: 'Gunpowder Golden Temple', steepingTime: 3},
+    {name: 'Assam Hatimara', steepingTime: 5},
+    {name: 'Bancha', steepingTime: 2},
+    {name: 'Ceylon New Vithanakande', steepingTime: 5},
+    {name: 'Golden Tip Yunnan', steepingTime: 5},
+    {name: 'Jasmine Phoenix Pearls', steepingTime: 3},
+    {name: 'Kenya Milima', steepingTime: 5},
+    {name: 'Pu Erh First Grade', steepingTime: 4},
+    {name: 'Sencha Makoto', steepingTime: 2},
+  ],
+};
+
+var TeaType = new GraphQLObjectType({
+  name: 'Tea',
+  fields: () => ({
+    name: {type: GraphQLString},
+    steepingTime: {type: GraphQLInt},
+  }),
+});
+
+var StoreType = new GraphQLObjectType({
+  name: 'Store',
+  fields: () => ({
+    teas: {type: new GraphQLList(TeaType)},
+  }),
+});
+
+export default new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: () => ({
+      store: {
+        type: StoreType,
+        resolve: () => STORE,
+      },
+    }),
+  }),
+});
